a722d6a8ec4519f8cf467fa67e62f6085262ec70
Shivam Git DB <aryansn0101@gmail.com>
2021-02-23 17:27:13 +0530

    new_commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css';
+import {data_list} from "./Data"
+
+
+
+function App() {
+
+
+
+
+
+  return (
+    <div className="App">
+
+
+    
+       
+
+        
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,7 @@
+export const data_list = [
+  {
+    "id": 1,
+    "title": "Photosnap",
+    "img": "./assets/web/1.jpg",
+  }
+]
